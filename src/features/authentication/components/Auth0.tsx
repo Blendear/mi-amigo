5fd3677b045f159cee8037ed723a82d2dde5bdc8
Blendear <tobiascibis@gmail.com>
2023-05-26 15:45:38 +0200

    Adding firebase - first prototype results

---
+++ src/features/authentication/components/Auth0.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "../lib/initFirebase";
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Redirect urlafter sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/testowa-stronka",
+  // auth providers, that let us log in to the app through them
+  signInOptions: [firebase.auth.GithubAuthProvider.PROVIDER_ID], //hook1 - add google
+};
+
+// Component for authentication throguh github, google etc (depending on what i add in "signInOptions")
+function Auth0Container() {
+  return (
+    <div
+      style={{
+        maxWidth: "320px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Pineapple Login</h1>
+      <p>Please sign-in:</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
+}
+
+export default Auth0Container;
---
+++ src/pages/testowa-stronka.tsx
@@ -0,0 +1,6 @@
+import styles from "src/styles/sass/styles-all.module.scss";
+
+const TestPage = () => {
+  return <div>Zalogowanyś. GG c:</div>;
+};
+export default TestPage;
