20ffd0f5cccac28c256d651434be9f619288afbf
Blendear <tobiascibis@gmail.com>
2023-05-27 20:50:22 +0200

    Added Firebas and Firestore - create and update functions added

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -5,7 +5,7 @@
 //
 //           _._._. AAA
 //
-//~~ _. Add Firebase / Firestore DataBase connection (for talking with your database, for example for CRUD operations)
+//~~ _. Use your connection with Firebase / Firestore DataBase for CRUD operations
 //
 //       _._. Connect to Firestore DB ("db" is our custom name - we gave it inside "init-firebase.ts") - for making CRUD operation on our database   &   import neede funtions to operate on colections, documents etc.
 //
@@ -13,11 +13,25 @@
 //
 //       _._. Get all Documents from chosen Collection   &   save their data
 //
-//           _._._. Get all Documents asynchronously and on every page load (useEffetc with empty dependencies array) from our specified Colleciton ("zakupki-i-zapasy")
+//           _._._. (CRUD - C) Create new Document to a Collection
 //
-//           _._._. Change the structure of the data (to "only sexy, readable data format & id of the document - aka documents name") - using build in firestore function "docs" property.   &   save it (set useState)
+//                  _._._._. Button - Activate "Create new Document" onClick
 //
-//           _._._. Render the data from Documents (example below)
+//           _._._. (CRUD - R) Get all Documents asynchronously and on every page load (useEffetc with empty dependencies array) from our specified Colleciton ("zakupki-i-zapasy")
+//
+//                  _._._._. Change the structure of the data (to "only sexy, readable data format & id of the document - aka documents name") - using build in firestore function "docs" property.   &   save it (set useState)
+//
+//                  _._._._. Render the data from Documents (example below)
+//
+//           _._._. (CRUD - U) Update new Document to a Collection - activated onClick (for example)
+//
+//                  _._._._. Get reference the document we want to update - by passing : databaseReference, collectionName, documentName
+//
+//                  _._._._. Create a variable for the updated data (for example a hardcoded string + a random number)
+//
+//                  _._._._. Get the build-in update function and add the updated data variable into it
+//
+//                  _._._._. Button with prop - Activate "Update new Document" onClick (I passed the document id as a prop, for the fun of it - can be added inside the function tho)
 //
 
 import styles from "src/styles/sass/styles-all.module.scss";
@@ -26,18 +40,35 @@ import { useState, useEffect } from "react";
 //       _._. Connect to Firestore DB ("db" is our custom name - we gave it inside "init-firebase.ts") - for making CRUD operation on our database   &   import neede funtions to operate on colections, documents etc.
 //
 import { db } from "../features/authentication/lib/init-firebase";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection, // gets a reference to a chosen collection
+  getDocs,
+  updateDoc,
+  doc, // gets a reference to a chosen document
+  deleteDoc,
+} from "firebase/firestore";
 
 const LandingPage = () => {
   //
   //       _._. Get all Documents from chosen Collection   &   save their data
   //
   const [dataFromDB, setDataFromDB] = useState([]);
-  //
-  //           _._._. Get all Documents asynchronously and on every page load (useEffetc with empty dependencies array) from our specified Colleciton ("zakupki-i-zapasy")
-  //
+
   const collectionReference = collection(db, "zakupki-i-zapasy");
 
+  //
+  //           _._._. (CRUD - C) Create new Document to a Collection - activated onClick (for example)
+  //
+  const createDocumentInsideASpecificCollection = async () => {
+    await addDoc(collectionReference, {
+      giereczka: "overwatch",
+      książeczka: "Archiwum Burzowego Światła",
+    });
+  };
+  //
+  //           _._._. (CRUD - R) Get all Documents asynchronously and on every page load (useEffetc with empty dependencies array) from our specified Colleciton ("zakupki-i-zapasy")
+  //
   useEffect(() => {
     console.log("useEffect started");
     //hook1 - refacotr this into 3rd party component \/ it makes a lot of code in this index.ts file
@@ -45,7 +76,7 @@ const LandingPage = () => {
       const allDocuments = await getDocs(collectionReference);
       console.log("our data : ", allDocuments);
       //
-      //           _._._. Change the structure of the data (to "only sexy, readable data format & id of the document - aka documents name") - using build in firestore function "docs" property.   &   save it (set useState)
+      //                  _._._._. Change the structure of the data (to "only sexy, readable data format & id of the document - aka documents name") - using build in firestore function "docs" property.   &   save it (set useState)
       //
       setDataFromDB(
         allDocuments.docs.map((document) => ({
@@ -57,25 +88,76 @@ const LandingPage = () => {
 
     getAllDocumentsFromASpecificCollection();
   }, []);
+  //
+  //           _._._. (CRUD - U) Update new Document to a Collection - activated onClick (for example)
+  //
+  const updateASpecificDocumentInsideASpecificCollection = async (
+    documentID
+  ) => {
+    //
+    //                  _._._._. Get reference the document we want to update - by passing : databaseReference, collectionName, documentName
+    //
+    const exampleDocumentReference = doc(
+      db, //which database
+      "zakupki-i-zapasy", // which collection - trap1 - dont add "collecitonReference" here, because its not a string. and "doc()" acceptcs only strings. you could create dedicated state for storing those strings tho
+      documentID // which document
+    );
+    //
+    //                  _._._._. Create a variable for the updated data (for example a hardcoded string + a random number)
+    //
+    const newUpdatedData = "update'owane żarełko - update nr".concat(
+      (Math.random() * 100).toFixed(1)
+    );
+    //
+    //                  _._._._. Get the build-in update function and add the updated data variable into it
+    //
+    await updateDoc(exampleDocumentReference, {
+      żarełko: newUpdatedData,
+    });
+  };
 
   return (
     <div>
       <div>landing page</div>
       {/* 
-      //           _._._. Render the data from Documents (example below)
+      //                  _._._._. Render the data from Documents (example below)
       */}
       <div>
         {dataFromDB.map((document, index) => {
           return (
             <div key={index}>
               <div>______________</div>
-              <div>ID Dokumentu : {document.id}</div>
               <div>
-                Przykładowa pozycja danych z dokumentu : {document.żarełko}
+                {`ID Dokumentu : `}
+                {`( ${document.id} )`}
+              </div>
+              <div>
+                {`Property "żarełko" Dokumentu : `}
+                {`[ ${document.żarełko} ]`}
               </div>
             </div>
           );
         })}
+      </div>
+      {/* 
+      //                  _._._._. Button - Activate "Create new Document" onClick
+      */}
+      <div>
+        <button onClick={createDocumentInsideASpecificCollection}>
+          CREATE Dokument Placeholderowy w DB
+        </button>
+      </div>
+      {/* 
+      //                  _._._._. Button with prop - Activate "Update new Document" onClick (I passed the document id as a prop, for the fun of it - can be added inside the function tho)
+      */}
+      <div>
+        <button
+          onClick={() => {
+            updateASpecificDocumentInsideASpecificCollection("tobi-i-zuza");
+          }}
+        >
+          {`UPDATE konkretny Dokument w DB (np. doc o ID "tobi-i-zuza", wartość jego property "żarełko")`}
+        </button>
       </div>
     </div>
   );
