12e24e2f45dd0ec1b46b9d4e716a14f7ef7df0bb
Blendear <tobiascibis@gmail.com>
2023-05-31 21:38:25 +0200

    Added raw loading-page main content

--- src/features/list-of-shops-the-user-needs/components/ShopToVisitSingle.jsx
+++ src/features/list-of-shops-the-user-needs/components/ShopToVisitSingle.jsx
@@ -9,26 +9,28 @@
 import styles from "src/styles/sass/styles-all.module.scss";
 import Image from "next/image";
 
-const ShopToVisitSingle = () => {
+const ShopToVisitSingle = ({ isActive }) => {
   return (
-    <div
-      // className={styles["container"]}   -   uncomment after placeholder \/ isnt needed
-      style={{ position: "relative", height: "auto", width: "6rem" }}
-    >
-      {/* /\ jemu daj "position:relative" */}
-      <div className={styles["image-styling"]}>
-        {/* /\ jemu daj "position:relative"   &   jemu przypisz classe" */}
-        <Image
-          src={`/images/testing/kod.png`}
-          alt={`error while loading image`}
-          height={200}
-          width={200}
-          layout="responsive"
-          objectFit="cover"
-        />
-        {/* /\ jemu zadnej classy nie dajesz*/}
+    isActive && (
+      <div
+        // className={styles["container"]}   -   uncomment after placeholder \/ isnt needed
+        style={{ position: "relative", height: "auto", width: "6rem" }}
+      >
+        {/* /\ jemu daj "position:relative" */}
+        <div className={styles["image-styling"]}>
+          {/* /\ jemu daj "position:relative"   &   jemu przypisz classe" */}
+          <Image
+            src={`/images/testing/kod.png`}
+            alt={`error while loading image`}
+            height={200}
+            width={200}
+            layout="responsive"
+            objectFit="cover"
+          />
+          {/* /\ jemu zadnej classy nie dajesz*/}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 export default ShopToVisitSingle;
