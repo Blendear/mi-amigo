7b408f952c17df54c7a8343d006532238c8b53bd
Blendear <tobiascibis@gmail.com>
2023-06-21 11:57:47 +0200

    fix: add TS specific fixes

--- src/features/list-of-shops-the-user-needs/components/ShopToVisitSingle.tsx
+++ src/features/list-of-shops-the-user-needs/components/ShopToVisitSingle.tsx
@@ -7,18 +7,20 @@ import { ShopToVisitProps } from "../types";
 
 const ShopToVisitSingle = ({ isActive, shopURL }: ShopToVisitProps) => {
   return (
-    isActive && (
-      <Image
-        src={
-          shopURL
-            ? shopURL
-            : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPMffp6aWziH9CgHrxbJzUhIV5SEj0_8Y_yps_JPa4pwPMDSAX2RbVsV5seOR32NOBXfI&usqp=CAU"
-        }
-        alt={`can't load image`}
-        layout="fill"
-        objectFit="cover"
-      />
-    )
+    <>
+      {isActive && (
+        <Image
+          src={
+            shopURL
+              ? shopURL
+              : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPMffp6aWziH9CgHrxbJzUhIV5SEj0_8Y_yps_JPa4pwPMDSAX2RbVsV5seOR32NOBXfI&usqp=CAU"
+          }
+          alt={`can't load image`}
+          layout="fill"
+          objectFit="cover"
+        />
+      )}
+    </>
   );
 };
 export default ShopToVisitSingle;
