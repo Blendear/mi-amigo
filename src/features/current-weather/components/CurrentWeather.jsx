9b9d7e1e9fc8a6d8c4911ae5fee3287f5446b652
Blendear <tobiascibis@gmail.com>
2023-06-22 20:40:11 +0200

    feature: show weather icon based on live weather conditions

--- src/features/current-weather/components/CurrentWeather.jsx
+++ src/features/current-weather/components/CurrentWeather.jsx
@@ -13,6 +13,7 @@ const CurrentWeather = () => {
       temp: 29.05,
       feels_like: 29.49,
     },
+    weather: [{ description: "asdads" }],
   }); // placeholder, needed before the actual data gets fetched
 
   useEffect(() => {
@@ -21,7 +22,11 @@ const CurrentWeather = () => {
 
   return (
     <div className={styles["landing-page__weather-container"]}>
-      <DynamicWeatherIcon />
+      <DynamicWeatherIcon
+        weatherDescription={weatherData.weather[0].description}
+      />
+      {console.log(weatherData.weather[0].description)}
+      {/* <div>{weatherData.weather[0].description}</div> */}
       <Temperature temperature={weatherData.main.temp.toFixed(1)} />
     </div>
   );
--- src/features/current-weather/components/DynamicWeatherIcon.jsx
+++ src/features/current-weather/components/DynamicWeatherIcon.jsx
@@ -3,10 +3,27 @@
 //
 import styles from "src/styles/sass/styles-all.module.scss";
 
-import { TiWeatherSunny, TiWeatherPartlySunny } from "react-icons/ti";
+import {
+  TiWeatherSunny,
+  TiWeatherPartlySunny,
+  TiWeatherCloudy,
+  TiWeatherShower,
+  TiWeatherStormy,
+  TiWeatherSnow,
+} from "react-icons/ti";
 
-const DynamicWeatherIcon = () => {
-  return <TiWeatherPartlySunny size="80%" color="#c08285" />;
+const DynamicWeatherIcon = ({ weatherDescription }) => {
+  return {
+    "clear sky": <TiWeatherSunny size="80%" color="#c08285" />,
+    "few clouds": <TiWeatherPartlySunny size="80%" color="#c08285" />,
+    "scattered clouds": <TiWeatherCloudy size="80%" color="#c08285" />,
+    "broken clouds": <TiWeatherCloudy size="80%" color="#c08285" />,
+    "shower rain": <TiWeatherShower size="80%" color="#c08285" />,
+    rain: <TiWeatherShower size="80%" color="#c08285" />,
+    thunderstorm: <TiWeatherStormy size="80%" color="#c08285" />,
+    snow: <TiWeatherSnow size="80%" color="#c08285" />,
+    mist: <TiWeatherCloudy size="80%" color="#c08285" />,
+  }[weatherDescription];
 };
 export default DynamicWeatherIcon;
 
